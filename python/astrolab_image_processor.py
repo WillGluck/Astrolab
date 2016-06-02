83f4f9df9d449effbc8aefa7937fd811380ce996
Will Glück <bigwillgluck@gmail.com>
2016-06-02 04:37:04 -0300

    Python added

---
+++ python/astrolab_image_processor.py
@@ -0,0 +1,32 @@
+
+class AstrolabImageProcessor:
+
+    def denoise(image):
+        image = Mat(image, Rect(106, 106, 212, 212))
+
+        gray = Mat()
+		binary = Mat()
+		finalImage = Mat()
+
+		hierarchy = Mat()
+		contours = []
+
+		biggerContour = None
+
+		cv2.cvtColor(image, gray, cv2.COLOR_BGR2GRAY)
+		cv2.threshold(gray, binary, 20, 255, cv2.THRESH_BINARY)
+		cv2.findContours(binary, contours, hierarchy, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+
+		for contour in contours:
+			area = cv2.contourArea(contour)
+			if area < pow(10, 2) or pow(10, 2) < area:
+				if biggerContour == None or cv2.contourArea(contour) > cv2.contourArea(biggerContour)
+					biggerContour = contour
+
+		maskImage = Mat(image.size(), CvType.CV_8U, Scalar(0));
+		cv2.drawContours(maskImage, contours, contours.indexOf(biggerContour), Scalar(255), Core.FILLED)
+
+		gray.copyTo(finalImage, maskImage)
+		finalImage.convertTo(finalImage, -1, 1.1, 0)
+
+		return finalImage;
---
+++ python/main.py
@@ -0,0 +1,11 @@
+import os
+import numpy as np
+import cv2
+import tensorflow
+
+img = cv2.imread('/media/willgluck/a2aa6a5f-a88a-45c7-af45-d38ccf2b7639/work/100008.jpg', 0)
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+print("Teste")
