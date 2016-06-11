112e75767db22ac37c4edcc06f798c3f1550dc88
Will Gluck <bigwillgluck@gmail.com>
2016-06-10 21:35:50 -0300

    Fix

--- python/astrolab_neural_network.py
+++ python/astrolab_neural_network.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from data import DataWrapper
+from matplotlib import pyplot as plt
 import numpy as np
 
 from tensorflow.examples.tutorials.mnist import input_data
@@ -7,34 +8,47 @@ from tensorflow.examples.tutorials.mnist import input_data
 class AstrolabNeuralNetwork:
 
     def __init__(self):
+    
         self.session = tf.InteractiveSession()
+        self.predicted_y = None        
+        
+        self.input_shape = 112
+        self.input_shape_conved = int(self.input_shape / 4)
+        self.input_dimension = self.input_shape * self.input_shape
+        self.output_dimension = 3
+        
+    def load_graph(self, image):
+        print("temp")
+        
+    
+    def classify(self, image):
+        print("test")       
+        
+        saver = tf.train.Saver()
+        saver.restore(self.session, "./trained_variables.ckpt")
+         
+        classification =  sess.run(tf.argmax(self.predicted_y, 1), feed_dict=[x:image])
+        classification = sess.run(tf.argmax(y, 1), feed_dict={x: [img]})
+        
+        plt.imshow(image.reshape(112, 112), cmap=plt.cm.binary)
+        plt.show()
+        
+        print 'NN predicted', classification[0]
 
 
-    def create(self):
-
-        #INIT
-
-        input_shape = 112
-        input_shape_conved = int(input_shape / 4)
-        input_dimension = input_shape * input_shape
-        output_dimension = 3
-
+    def train(self, data_wrapper):
 
         #BatchSize and Image size
-        x = tf.placeholder(tf.float32, shape=[None, input_dimension])
+        x = tf.placeholder(tf.float32, shape=[None, self.input_dimension])
         #Output size
-        correct_y = tf.placeholder(tf.float32, shape=[None, output_dimension])
+        correct_y = tf.placeholder(tf.float32, shape=[None, self.output_dimension])
 
         #LOAD MNIST
+        
         training_images = None
         test_images = None
         training_labels = None
         test_labels = None
-
-        #data = input_data.read_data_sets('MNIST_data', one_hot=True)
-        data = DataWrapper()
-        data.train.load_labels('/media/willgluck/a2aa6a5f-a88a-45c7-af45-d38ccf2b7639/work/Galaxies/labels.csv')
-        data.train.load_images_names('/media/willgluck/a2aa6a5f-a88a-45c7-af45-d38ccf2b7639/work/Galaxies/images/', input_dimension)
 
         #TODO
 
@@ -45,7 +59,7 @@ class AstrolabNeuralNetwork:
         #32 - output channels
         conv1_b = self.create_random_biases([32])
         #reshape for tensor
-        x_reshaped = tf.reshape(x, [-1, input_shape, input_shape, 1])
+        x_reshaped = tf.reshape(x, [-1, self.input_shape, self.input_shape, 1])
         #Convolv and apply bias.
         conv1_o = tf.nn.relu(self.do_conv2d(x_reshaped, conv1_W) + conv1_b)
         pool1_o = self.do_max_pool_2x2(conv1_o)
@@ -60,10 +74,10 @@ class AstrolabNeuralNetwork:
 
         #THIRD LAYER - Connected layer
 
-        d1_W = self.create_random_weights([input_shape_conved * input_shape_conved * 64, 1024])
+        d1_W = self.create_random_weights([self.input_shape_conved * self.input_shape_conved * 64, 1024])
         d1_b = self.create_random_biases([1024])
 
-        pool2_o_reshaped = tf.reshape(pool2_o, [-1, input_shape_conved * input_shape_conved * 64])
+        pool2_o_reshaped = tf.reshape(pool2_o, [-1, self.input_shape_conved * self.input_shape_conved * 64])
         d1_o = tf.nn.relu(tf.matmul(pool2_o_reshaped, d1_W) + d1_b)
 
         #Dropout output to avoid overfitting
@@ -72,23 +86,23 @@ class AstrolabNeuralNetwork:
 
         #FINAL LAYER - Softmax
 
-        d2_W = self.create_random_weights([1024, output_dimension])
-        d2_b = self.create_random_biases([output_dimension])
+        d2_W = self.create_random_weights([1024, self.output_dimension])
+        d2_b = self.create_random_biases([self.output_dimension])
 
-        predicted_y = tf.nn.softmax(tf.matmul(d1_o_dropout, d2_W) + d2_b)
+        self.predicted_y = tf.nn.softmax(tf.matmul(d1_o_dropout, d2_W) + d2_b)
 
         #TRAIN
 
-        cross_entropy = tf.reduce_mean(-tf.reduce_sum(correct_y * tf.log(predicted_y), reduction_indices=[1]))
+        cross_entropy = tf.reduce_mean(-tf.reduce_sum(correct_y * tf.log(self.predicted_y), reduction_indices=[1]))
         train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-        correct_prediction = tf.equal(tf.argmax(predicted_y, 1), tf.argmax(correct_y, 1))
+        correct_prediction = tf.equal(tf.argmax(self.predicted_y, 1), tf.argmax(correct_y, 1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
         self.session.run(tf.initialize_all_variables())
         saver = tf.train.Saver()
 
         for i in range(20000):
 
-            batch = data.train.next_batch(10)
+            batch = data_wrapper.train.next_batch(10)
 
             if i % 100 == 0:
                 train_accuracy = accuracy.eval(feed_dict = {
@@ -105,7 +119,7 @@ class AstrolabNeuralNetwork:
             })
 
         #saver.restore(self.session, "./trained_variables.ckpt")
-        batch = data.train.next_batch(1000)
+        batch = data_wrapper.train.next_batch(1000)
 
         print("Test accuracy %g"%accuracy.eval(feed_dict={
             x: batch[0],
--- python/main.py
+++ python/main.py
@@ -25,6 +25,12 @@ for file_name in origin_file_name_list:
     img = image_processor.denoise(img)
     cv2.imwrite(os.path.join(denoised_training_folder, file_name), img)
 
+
+#data = input_data.read_data_sets('MNIST_data', one_hot=True)
+data = DataWrapper()
+data.train.load_labels('/media/willgluck/a2aa6a5f-a88a-45c7-af45-d38ccf2b7639/work/Galaxies/labels.csv')
+data.train.load_images_names('/media/willgluck/a2aa6a5f-a88a-45c7-af45-d38ccf2b7639/work/Galaxies/images/', input_dimension)
+
 # neural_network = AstrolabNeuralNetwork()
 # neural_network.create()
 
